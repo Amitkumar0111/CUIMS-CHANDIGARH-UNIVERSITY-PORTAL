99199d137fcdb38132e5e0b273f78eb259322427
Amit kumar <kr.mayank2021@gmail.com>
2025-04-09 00:53:12 +0530

    Initial commit

---
+++ staff-login/assets/js/script.js
@@ -0,0 +1,8 @@
+function sweet() {
+    Swal.fire({
+        icon: "success",
+        title: "Done",
+        text: "Redirecting to next page...",
+        footer: '<a href="https://www.youtube.com/watch?v=gmyRw66w4uA">No Issues</a>'
+    });
+}
